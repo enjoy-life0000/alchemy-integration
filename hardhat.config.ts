de6d1cd03b444aa4f4724ec9cab09d6d64d73562
enjoy-life0000 <enjoylife.follower@gmail.com>
2025-01-06 03:36:33 -0500

    update hardhat config(fix .env)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
+dotenv.config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "sepolia",
+   networks: {
+      hardhat: {},
+      sepolia: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
 };
 
 export default config;
